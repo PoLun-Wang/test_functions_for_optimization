297f533bf641aaee17447e0fa804df5587d331aa
Po-Lun Wang <35827144+PoLun-Wang@users.noreply.github.com>
2018-10-27 13:36:29 +0800

    Revert "Merge pull request #5 from lovecolala/master"
    This reverts commit 0e34f194b96b46aee199bb4fe8127800342d1d00.

--- Get_Functions_details.m
+++ Get_Functions_details.m
@@ -24,7 +24,7 @@
 % up is the uppper bound: ub=[ub_1,ub_2,...,ub_d]
 % dim is the number of variables (dimension of the problem)
 
-function [lb,ub,dim,fobj, bestSol] = Get_Functions_details(F)
+function [lb,ub,dim,fobj,bestSol] = Get_Functions_details(F)
 
 
 switch F
@@ -190,7 +190,6 @@ switch F
                 bestSol = -10.5363;
                 
         case 'F24'
-                % Power
                 fobj = @F24;
                 lb = 0;
                 ub = 10;
@@ -198,7 +197,6 @@ switch F
                 bestSol = 0;
                 
         case 'F25'
-                % Zakharov
                 fobj = @F25;
                 lb = -100;
                 ub = 100;
@@ -206,7 +204,6 @@ switch F
                 bestSol = 0;
                 
         case 'F26'
-                % Matyas
                 fobj = @F26;
                 lb = -100;
                 ub = 100;
@@ -214,44 +211,70 @@ switch F
                 bestSol = 0;
                 
         case 'F27'
-                % Perm d, beta
                 fobj = @F27;
                 lb = -100;
                 ub = 100;
                 dim = 100;
                 bestSol = 0;
                 
-        case 'F28'
-                fobj = @F28;
-                lb = 0;
+        case 'F29'
+            fobj = @F29;
+            lb = -500;
+            ub = 500;
+            dim = 100;
+            bestSol = 0;
+                  
+        case 'F34'
+                fobj = @F34;
+                lb = -100;
+                ub = 100;
+                dim = 100;
+                bestSol = 0;
+                
+        case 'F35'
+                fobj = @F35;
+                lb = -5;
+                ub = 5;
+                dim = 100;
+                bestSol = -39.16599*dim;
+                
+        case 'F36'
+                fobj = @F36;
+                lb = -5;
+                ub = 5;
+                dim = 100;
+                bestSol = 0;
+        
+        case 'F37'
+                fobj = @F37;
+                lb = -10;
                 ub = 10;
                 dim = 100;
-                bestSol = -100;
+                bestSol = -186.7309;
                 
-        case 'F29'
-                fobj = @F29;
-                lb = -0.5;
-                ub = 0.5;
+        case 'F38'
+                fobj = @F38;
+                lb = -10;
+                ub = 10;
                 dim = 100;
-                bestSol = 0;                
-                
+                bestSol = 0;
 end
 
 end
 
-% F1
+% F1 : Sphere Function
 
 function o = F1(x)
 o=sum(x.^2);
 end
 
-% F2
+% F2 : Schwefel Function 2.22
 
 function o = F2(x)
 o=sum(abs(x))+prod(abs(x));
 end
 
-% F3
+% F3 : Schwefel Function 1.2
 
 function o = F3(x)
 dim=size(x,2);
@@ -261,60 +284,60 @@ for i=1:dim
 end
 end
 
-% F4
+% F4 : Schwefel Function 2.21
 
 function o = F4(x)
 o=max(abs(x));
 end
 
-% F5
+% F5 : Rosenbrock Function
 
 function o = F5(x)
 dim=size(x,2);
 o=sum(100*(x(2:dim)-(x(1:dim-1).^2)).^2+(x(1:dim-1)-1).^2);
 end
 
-% F6
+% F6 :  Step Function
 
 function o = F6(x)
 o=sum(abs((x+.5)).^2);
 end
 
-% F7
+% F7 : Quartic Function
 
 function o = F7(x)
 dim=size(x,2);
 o=sum([1:dim].*(x.^4))+rand;
 end
 
-% F8
+% F8 : Schwefel Function
 
 function o = F8(x)
 o=sum(-x.*sin(sqrt(abs(x))));
 end
 
-% F9
+% F9 : Rastrigin Function
 
 function o = F9(x)
 dim=size(x,2);
 o=sum(x.^2-10*cos(2*pi.*x))+10*dim;
 end
 
-% F10
+% F10 : Ackley Function
 
 function o = F10(x)
 dim=size(x,2);
 o=-20*exp(-.2*sqrt(sum(x.^2)/dim))-exp(sum(cos(2*pi.*x))/dim)+20+exp(1);
 end
 
-% F11
+% F11 : Griewank Function
 
 function o = F11(x)
 dim=size(x,2);
 o=sum(x.^2)/4000-prod(cos(x./sqrt([1:dim])))+1;
 end
 
-% F12
+% F12 : Penalized 1 Function
 
 function o = F12(x)
 dim=size(x,2);
@@ -322,7 +345,7 @@ o=(pi/dim)*(10*((sin(pi*(1+(x(1)+1)/4)))^2)+sum((((x(1:dim-1)+1)./4).^2).*...
         (1+10.*((sin(pi.*(1+(x(2:dim)+1)./4)))).^2))+((x(dim)+1)/4)^2)+sum(Ufun(x,10,100,4));
 end
 
-% F13
+% F13 : Penalized 2 Function
 
 function o = F13(x)
 dim=size(x,2);
@@ -330,7 +353,7 @@ o=.1*((sin(3*pi*x(1)))^2+sum((x(1:dim-1)-1).^2.*(1+(sin(3.*pi.*x(2:dim))).^2))+.
         ((x(dim)-1)^2)*(1+(sin(2*pi*x(dim)))^2))+sum(Ufun(x,5,100,4));
 end
 
-% F14
+% F14 : Shekel's Foxholes Function
 
 function o = F14(x)
 aS=[-32 -16 0 16 32 -32 -16 0 16 32 -32 -16 0 16 32 -32 -16 0 16 32 -32 -16 0 16 32;,...
@@ -342,7 +365,7 @@ end
 o=(1/500+sum(1./([1:25]+bS))).^(-1);
 end
 
-% F15
+% F15 : Kowalik's Function
 
 function o = F15(x)
 aK=[.1957 .1947 .1735 .16 .0844 .0627 .0456 .0342 .0323 .0235 .0246];
@@ -350,26 +373,26 @@ bK=[.25 .5 1 2 4 6 8 10 12 14 16];bK=1./bK;
 o=sum((aK-((x(1).*(bK.^2+x(2).*bK))./(bK.^2+x(3).*bK+x(4)))).^2);
 end
 
-% F16
+% F16 : Six-Hump Camel-Back Function
 
 function o = F16(x)
 o=4*(x(1)^2)-2.1*(x(1)^4)+(x(1)^6)/3+x(1)*x(2)-4*(x(2)^2)+4*(x(2)^4);
 end
 
-% F17
+% F17 : Branin Function
 
 function o = F17(x)
 o=(x(2)-(x(1)^2)*5.1/(4*(pi^2))+5/pi*x(1)-6)^2+10*(1-1/(8*pi))*cos(x(1))+10;
 end
 
-% F18
+% F18 : Goldstein-Price Function
 
 function o = F18(x)
 o=(1+(x(1)+x(2)+1)^2*(19-14*x(1)+3*(x(1)^2)-14*x(2)+6*x(1)*x(2)+3*x(2)^2))*...
         (30+(2*x(1)-3*x(2))^2*(18-32*x(1)+12*(x(1)^2)+48*x(2)-36*x(1)*x(2)+27*(x(2)^2)));
 end
 
-% F19
+% F19 : Hartman's Family
 
 function o = F19(x)
 aH=[3 10 30;.1 10 35;3 10 30;.1 10 35];cH=[1 1.2 3 3.2];
@@ -380,7 +403,7 @@ for i=1:4
 end
 end
 
-% F20
+% F20 : Hartman's Family
 
 function o = F20(x)
 aH=[10 3 17 3.5 1.7 8;.05 10 17 .1 8 14;3 3.5 1.7 10 17 8;17 8 .05 10 .1 14];
@@ -393,7 +416,7 @@ for i=1:4
 end
 end
 
-% F21
+% F21 : Shekel's Family
 
 function o = F21(x)
 aSH=[4 4 4 4;1 1 1 1;8 8 8 8;6 6 6 6;3 7 3 7;2 9 2 9;5 5 3 3;8 1 8 1;6 2 6 2;7 3.6 7 3.6];
@@ -405,7 +428,7 @@ for i=1:5
 end
 end
 
-% F22
+% F22 : Shekel's Family
 
 function o = F22(x)
 aSH=[4 4 4 4;1 1 1 1;8 8 8 8;6 6 6 6;3 7 3 7;2 9 2 9;5 5 3 3;8 1 8 1;6 2 6 2;7 3.6 7 3.6];
@@ -417,7 +440,7 @@ for i=1:7
 end
 end
 
-% F23
+% F23 : Shekel's Family
 
 function o = F23(x)
 aSH=[4 4 4 4;1 1 1 1;8 8 8 8;6 6 6 6;3 7 3 7;2 9 2 9;5 5 3 3;8 1 8 1;6 2 6 2;7 3.6 7 3.6];
@@ -456,8 +479,6 @@ term2 = prod(x);
 o = 0.26 .* term1 - 0.48 .* term2;
 end
 
-% F27
-
 function o = F27(x)
 b = 0.5;
 d = length(x);
@@ -465,27 +486,95 @@ inner = sum(((1:d).^((1:d)') + b) .* ((x./(1:d)).^((1:d)')-1), 2)';
 o = sum(inner .^2);
 end
 
-% F28
-
-function o = F28(x)
-o = -sum(sin(10.*log2(x)));
-end
-
-% F29
-
+% F 29 : Qing Function
+% ref : http://benchmarkfcns.xyz/benchmarkfcns/qingfcn.html
 function o = F29(x)
-kmax = 20;
-a = 0.5;
-b = 3;
-d = length(x);
-o = 0;
-for i = 1:d
-        sum1 = sum(a.^(0:kmax) .* cos(2*pi.*b.^(0:kmax).*(x(i)+0.5)));
-        sum2 = d .* sum(a.^(0:kmax) .* cos(pi.*b.^(0:kmax)));
-        o = o + sum1 - sum2;
-end
+    n = size(x, 2);
+    x2 = x .^2;
+    
+    scores = 0;
+    for i = 1:n
+        scores = scores + (x2(:, i) - i) .^ 2;
+    end
+    o = scores;
 end
 
+% F34: Salomon Function
+% ref: http://benchmarkfcns.xyz/benchmarkfcns/salomonfcn.html
+function o = F34(x)
+    x2 = x .^ 2;
+    sumx2 = sum(x2, 2);
+    sqrtsx2 = sqrt(sumx2);
+    
+    scores = 1 - cos(2 .* pi .* sqrtsx2) + (0.1 * sqrtsx2);
+    
+    o = scores;
+end
+
+% F35: Styblinski-Tank Function
+% ref: http://benchmarkfcns.xyz/benchmarkfcns/styblinskitankfcn.html
+function o = F35(x)
+    n = size(x, 2);
+    scores = 0;
+    for i = 1:n
+        scores = scores + ((x(:, i) .^4) - (16 * x(:, i) .^ 2) + (5 * x(:, i)));
+    end
+    scores = 0.5 * scores;
+    
+    o = scores;
+end
+
+% F36: Xin-She Yang Function
+% ref: http://benchmarkfcns.xyz/benchmarkfcns/xinsheyangn1fcn.html
+function o = F36(x)
+    n = size(x, 2);
+
+    scores = 0;
+    for i = 1:n
+        scores = scores + rand * (abs(x(:, i)) .^ i);
+    end
+    
+    o = scores;
+end
+
+% F37: Shubert Function
+% ref: http://benchmarkfcns.xyz/benchmarkfcns/shubertfcn.html
+function o = F37(x)
+    n = size(x, 2);
+    
+    scores = 1;
+    for i = 1:n
+        inner_sum = 0;
+        for j = 1:5
+            inner_sum = inner_sum + j * cos(((j + 1) * x(:, i)) + j);
+        end
+        scores = inner_sum .* scores;
+    end
+    
+    o = scores;
+end
+
+% F38: Levy Function
+% ref: https://www.sfu.ca/~ssurjano/levy.html
+function o = F38(x)
+    d = size(x, 2);
+
+    for ii = 1:d
+        w(ii) = 1 + (x(ii) - 1)/4;
+    end
+
+    term1 = (sin(pi*w(1)))^2;
+    term3 = (w(d)-1)^2 * (1+(sin(2*pi*w(d)))^2);
+
+    sum = 0;
+    for ii = 1:(d-1)
+        wi = w(ii);
+            new = (wi-1)^2 * (1+10*(sin(pi*wi+1))^2);
+        sum = sum + new;
+    end
+
+    o = term1 + sum + term3;
+end
 
 function o=Ufun(x,a,k,m)
 o=k.*((x-a).^m).*(x>a)+k.*((-x-a).^m).*(x<(-a));
